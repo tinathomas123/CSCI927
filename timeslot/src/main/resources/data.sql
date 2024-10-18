fb453217058183856b3cf52c041de1e21f0c046f
tinathomas123 <christinethomas221@gmail.com>
2024-10-18 11:23:36 +1100

    removing files outside the new folder

--- timeslot/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO TIMESLOT(date_time, day, selected_table) values('2024-11-10 15:30:00', 'Sunday', 'T1');
-INSERT INTO TIMESLOT(date_time, day, selected_table) values('2024-11-12 17:30:00', 'Tuesday', 'T1');
-
-INSERT INTO TIMESLOT(date_time, day, selected_table) values( '2024-11-09 16:30:00', 'Saturday', 'T2');
-INSERT INTO TIMESLOT(date_time, day, selected_table) values( '2024-11-11 18:30:00', 'Monday', 'T2');
-
-INSERT INTO TIMESLOT(date_time, day, selected_table) values( '2024-11-13 19:30:00', 'Wednesday', 'T3');
-INSERT INTO TIMESLOT(date_time, day, selected_table) values( '2024-11-14 20:30:00', 'Thursday', 'T3');
--- timeslot/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE TIMESLOT;
-
-create table TIMESLOT(
-day CHAR(15) NOT NULL,
-date_time DATETIME,
-selected_table CHAR(5),
-CONSTRAINT timeslot_pkey PRIMARY KEY(day, date_time, selected_table)
-);
