d23c65c73207c0463968c5b9f66adf29a543e681
tinathomas123 <christinethomas221@gmail.com>
2024-10-18 11:36:18 +1100

    removing files outside folder

--- available-venues/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO VENUE values('V1', 'Majestic Manor Hotel', 'Available');
-INSERT INTO VENUE values('V2', 'Serenity Sands Resort', 'Reserved');
-INSERT INTO VENUE values('V3', 'Lakeside Haven Resort', 'Available');
-
--- timeslot/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO TIMESLOT(date_time, day, selected_venue) values('2024-11-10 15:30:00', 'Sunday', 'V1');
-INSERT INTO TIMESLOT(date_time, day, selected_venue) values('2024-11-12 17:30:00', 'Tuesday', 'V1');
-
-INSERT INTO TIMESLOT(date_time, day, selected_venue) values( '2024-11-09 16:30:00', 'Saturday', 'V2');
-INSERT INTO TIMESLOT(date_time, day, selected_venue) values( '2024-11-11 18:30:00', 'Monday', 'V2');
-
-INSERT INTO TIMESLOT(date_time, day, selected_venue) values( '2024-11-13 19:30:00', 'Wednesday', 'V3');
-INSERT INTO TIMESLOT(date_time, day, selected_venue) values( '2024-11-14 20:30:00', 'Thursday', 'V3');
